170cfbe02badd4d19c435ee8b6260fd60f9fc9ba
Edy <edyenumo@hotmail.com>
2020-05-29 15:20:17 -0300

    Criou o metodo main para teste

--- BrazillianStates.java
+++ BrazillianStates.java
@@ -8,6 +8,10 @@ import javafx.collections.ObservableList;
 
 public class BrazillianStates {
 
+	public static void main(String args[]){
+		System.out.println("Minha rua: "+getCepAddres("15053025")[1]);
+	}
+
     public static ObservableList<String> brazilianStates = FXCollections.observableArrayList(
             "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
             "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO");
